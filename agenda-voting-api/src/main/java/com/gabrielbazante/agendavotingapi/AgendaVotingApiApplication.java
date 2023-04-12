81c5dd14cccfa1d1f9e0bd9204ab38a2949cf2fb
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-12 01:47:50 -0300

    Commit inicial

---
+++ agenda-voting-api/src/main/java/com/gabrielbazante/agendavotingapi/AgendaVotingApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbazante.agendavotingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaVotingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaVotingApiApplication.class, args);
+	}
+
+}
---
+++ agenda-voting-api/src/test/java/com/gabrielbazante/agendavotingapi/AgendaVotingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbazante.agendavotingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaVotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
